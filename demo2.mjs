5e12f5981291362c21f33611ccf6894b2d27a5d1
KallemShirisha <71708933+KallemShirisha@users.noreply.github.com>
2021-06-09 19:34:10 +0530

    Delete demo2.mjs

--- demo2.mjs
+++
@@ -1,8 +0,0 @@
-import squareArea ,{circleArea,rectArea} from "./demo1.mjs"
-var r=20;
-console.log(circleArea(r));
-var side=10;
-var l=20;
-var b=30;
-console.log(squareArea(side));
-console.log(rectArea(l,b));
